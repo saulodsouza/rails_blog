17678aadeecb05a97a146eff9d7139e647a8986a
Saulo Ferreira de Souza <saulosouza@proton.me>
2025-02-26 23:33:24 -0300

    Add cover image

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
 
   root "blog_posts#index"
 
-  resources :blog_posts
+  resources :blog_posts do
+    resource :cover_image, only: [ :destroy ], module: :blog_posts
+  end
 end
